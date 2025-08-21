8f619ca0ef86fa4560525666823482fe5bb869a7
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-08-21 14:03:06 +0200

    feat(repository): add champion repository implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation(libs.sandwich.retrofit)
-
+    implementation(libs.sandwich)
+    implementation(libs.sandwich.ktor)
+    implementation(libs.ktor.client.core)
 }
---
+++ app/src/main/java/com/sofiane/leagueoflegends/data/repository/ChampionRepositoryImpl.kt
@@ -0,0 +1,23 @@
+package com.sofiane.leagueoflegends.data.repository
+
+import com.skydoves.sandwich.ApiResponse
+import com.skydoves.sandwich.ktor.getApiResponse
+import com.sofiane.leagueoflegends.domain.model.ChampionResponseModel
+import com.sofiane.leagueoflegends.domain.repository.ChampionRepository
+import io.ktor.client.HttpClient
+class ChampionRepositoryImpl(
+    private val httpClient: HttpClient
+) : ChampionRepository {
+    companion object {
+        // TODO : ajouter l'url dans un fichier .env
+        const val BASE_URL = "https://ddragon.leagueoflegends.com/cdn/15.16.1/data/fr_FR"
+    }
+    override suspend fun getAllChampions(): ApiResponse<ChampionResponseModel> {
+        return httpClient.getApiResponse("champion.json")
+    }
+
+
+    override suspend fun getChampionByName(name: String): ApiResponse<ChampionResponseModel> {
+        return httpClient.getApiResponse("$name.json")
+    }
+}
