b39d608a69729752d4e06df9da7861abf3e18f35
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-08-23 19:31:00 +0200

    docs(MvvmApp): add kdoc

--- app/src/main/java/com/sofiane/leagueoflegends/MvvmApp.kt
+++ app/src/main/java/com/sofiane/leagueoflegends/MvvmApp.kt
@@ -3,6 +3,15 @@ package com.sofiane.leagueoflegends
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+/**
+ * Classe Application principale.
+ *
+ * Annotée avec [HiltAndroidApp], elle sert de point d'entrée
+ * pour l'initialisation de la DI Dagger Hilt dans l'application.
+ *
+ * Cette classe est créée avant toute autre classe et reste
+ * vivante tant que l'application est en mémoire.
+ */
 @HiltAndroidApp
 class MvvmApp : Application() {
 }
