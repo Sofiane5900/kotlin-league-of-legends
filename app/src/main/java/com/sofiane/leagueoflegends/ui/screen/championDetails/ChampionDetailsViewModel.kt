d31e30197840dd662c3e84d855ea54de59209cbb
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-08-23 17:32:09 +0200

    feat(ChampionsDetailsViewModel): add args to fetch champions details

--- app/src/main/java/com/sofiane/leagueoflegends/ui/screen/championDetails/ChampionDetailsViewModel.kt
+++ app/src/main/java/com/sofiane/leagueoflegends/ui/screen/championDetails/ChampionDetailsViewModel.kt
@@ -1,23 +1,34 @@
 package com.sofiane.leagueoflegends.ui.screen.championDetails
 
 import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.navigation.toRoute
+import com.skydoves.sandwich.onFailure
+import com.skydoves.sandwich.onSuccess
 import com.sofiane.leagueoflegends.domain.model.ChampionModel
 import com.sofiane.leagueoflegends.domain.repository.ChampionRepository
+import com.sofiane.leagueoflegends.ui.navigation.ChampionDetails
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ChampionDetailsViewModel @Inject constructor(
-    private val championRepository: ChampionRepository
+    private val championRepository: ChampionRepository,
+    private val savedStateHandle: SavedStateHandle
 ) : ViewModel() {
     var champion = mutableStateOf<ChampionModel?>(null)
 
     init {
+
+        val args = savedStateHandle.toRoute<ChampionDetails>()
         viewModelScope.launch {
-            championRepository.getChampionByName( )
+            championRepository.getChampionByName(args.name)
+                .onSuccess { champion.value = data.champion.values.firstOrNull() }
+                // TODO: renvoyer un error message en cas de failure
+                .onFailure { }
         }
     }
 }
