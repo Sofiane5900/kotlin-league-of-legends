a0fa58619862c3b383ad13089b517065d71b1540
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-08-23 17:05:24 +0200

    feat: add ChampionDetailsViewModel file

---
+++ app/src/main/java/com/sofiane/leagueoflegends/ui/screen/championDetails/ChampionDetailsViewModel.kt
@@ -0,0 +1,23 @@
+package com.sofiane.leagueoflegends.ui.screen.championDetails
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sofiane.leagueoflegends.domain.model.ChampionModel
+import com.sofiane.leagueoflegends.domain.repository.ChampionRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ChampionDetailsViewModel @Inject constructor(
+    private val championRepository: ChampionRepository
+) : ViewModel() {
+    var champion = mutableStateOf<ChampionModel?>(null)
+
+    init {
+        viewModelScope.launch {
+            championRepository.getChampionByName()
+        }
+    }
+}
