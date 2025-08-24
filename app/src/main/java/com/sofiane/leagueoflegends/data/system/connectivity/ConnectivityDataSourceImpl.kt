4146ebf105ee41c826d3f44d03ce9f24024ed320
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-08-24 21:24:26 +0200

    feat(ConnecitivityDataSourceImpl): add isConnectedWifi helper method

---
+++ app/src/main/java/com/sofiane/leagueoflegends/data/system/connectivity/ConnectivityDataSourceImpl.kt
@@ -0,0 +1,52 @@
+package com.sofiane.leagueoflegends.data.system.connectivity
+
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.net.NetworkCapabilities.NET_CAPABILITY_INTERNET
+import android.net.NetworkCapabilities.NET_CAPABILITY_VALIDATED
+import android.net.NetworkCapabilities.TRANSPORT_WIFI
+import android.net.wifi.WifiManager
+import jakarta.inject.Inject
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+
+/**
+ * DataSource Android qui convertit les callbacks systèmes
+ * en un Flow<NetworkState> pour l'UI.
+ */
+class ConnectivityDataSourceImpl @Inject constructor(
+    private val connectivityManager: ConnectivityManager,
+    private val wifiManager: WifiManager
+) : ConnectivityDataSource {
+
+    override fun observeNetworkState(): Flow<NetworkState> {
+        return callbackFlow {
+            trySend()
+        }
+    }
+
+
+    /**
+     * Vérifie si [NetworkCapabilities] correspondentà une connexion Wi-Fi.
+     *
+     * Conditions :
+     * - Le réseau doit avoir la capacité [NET_CAPABILITY_INTERNET].
+     * - Le réseau doit être validé par le système ([NET_CAPABILITY_VALIDATED]).
+     * - Le transport doit être du Wi-Fi ([TRANSPORT_WIFI]).
+     *
+     * @receiver [NetworkCapabilities]? L’objet représentant les capacités du réseau.
+     * Peut être `null` si aucune info n’est disponible.
+     *
+     * @return `true` si connecté à un Wi-Fi valide avec Internet, sinon `false`.
+     */
+    private fun NetworkCapabilities?.isConnectedWifi(): Boolean {
+        if (this == null)
+        {
+            return false
+        }
+        val hasInternet = hasCapability(NET_CAPABILITY_INTERNET)
+        val isValidated = hasCapability(NET_CAPABILITY_VALIDATED)
+        val isWifi = hasTransport(TRANSPORT_WIFI)
+        return hasInternet && isValidated && isWifi
+    }
+}
