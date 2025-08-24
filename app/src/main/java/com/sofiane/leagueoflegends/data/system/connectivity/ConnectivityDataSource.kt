909f150e8c952662c0cb1f66e18f970629cb5d1b
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-08-24 21:50:53 +0200

    docs(ConnectivityDataSource): add kdoc

--- app/src/main/java/com/sofiane/leagueoflegends/data/system/connectivity/ConnectivityDataSource.kt
+++ app/src/main/java/com/sofiane/leagueoflegends/data/system/connectivity/ConnectivityDataSource.kt
@@ -2,7 +2,19 @@ package com.sofiane.leagueoflegends.data.system.connectivity
 
 import kotlinx.coroutines.flow.Flow
 
-/** Expose l'état réseau en temps réel à travers un [Flow]. */
+/**
+ * Source de données qui expose l’état de la connectivité réseau.
+ *
+ * Abstraction qui permet d’observer la connexion en temps réel
+ * (par exemple Wi-Fi ON/OFF, changement de SSID, intensité du signal).
+ */
 interface ConnectivityDataSource {
+    /**
+     * Observe en continu l’état réseau sous forme de [Flow].
+     * - Émet un [NetworkState] initial.
+     * - Réémet à chaque changement détecté.
+     *
+     * @return Un [Flow] de [NetworkState].
+     */
     fun observeNetworkState(): Flow<NetworkState>
 }
