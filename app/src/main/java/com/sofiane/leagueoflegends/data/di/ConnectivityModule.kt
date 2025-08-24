e16964c182a3acfb91e3dbdb90749e2728c35877
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-08-24 21:47:45 +0200

    feat(ConnectivityModule): add provideConnectivityDataSource fun

--- app/src/main/java/com/sofiane/leagueoflegends/data/di/ConnectivityModule.kt
+++ app/src/main/java/com/sofiane/leagueoflegends/data/di/ConnectivityModule.kt
@@ -3,11 +3,14 @@ package com.sofiane.leagueoflegends.data.di
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.wifi.WifiManager
+import com.sofiane.leagueoflegends.data.system.connectivity.ConnectivityDataSource
+import com.sofiane.leagueoflegends.data.system.connectivity.ConnectivityDataSourceImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -20,6 +23,16 @@ object ConnectivityModule {
     @Provides
     fun provideWifiManager(@ApplicationContext context: Context): WifiManager {
        return context.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideConnectivityDataSource(
+        cm: ConnectivityManager,
+        wifi: WifiManager
+    ): ConnectivityDataSource {
+        return ConnectivityDataSourceImpl(cm, wifi)
     }
 
 
